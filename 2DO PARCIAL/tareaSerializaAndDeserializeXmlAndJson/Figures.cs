925e0f96f38d019a0dc8a36aea6d9d7c1fd2883f
Luis Villalobos <ivillalobosrivera@yahoo.com>
2021-05-13 18:09:15 -0500

    coments in figures structure

--- 2DO PARCIAL/tareaSerializaAndDeserializeXmlAndJson/Figures.cs
+++ 2DO PARCIAL/tareaSerializaAndDeserializeXmlAndJson/Figures.cs
@@ -6,18 +6,27 @@ using System.Text.Json.Serialization;
 
 namespace tareaSerializaAndDeserializeXmlAndJson
 {
+    /// <summary>
+    /// Estructura para las figuras
+    /// </summary>
     public struct Figures
     {
+        /// <summary>
+        /// Constructor se encarga de crear e inicializar las instancias de la estructura
+        /// </summary>
+        /// <param name="name">Recibe el nombre de la instancia de la figura</param>
+        /// <param name="area">Recibe el area de la instancia de la figura</param>
+        /// <param name="color">Recibe el color de la instancia de la figura</param>
         public Figures(string name, decimal area, string color){
-            this.name = name;
+            this.name = name; //Se asigna la propiedad a la instancia que se crea
             this.area = area;
             this.color = color;
         }
-        [XmlAttribute("name")]
-        public string name { get; set; }
+        [XmlAttribute("name")] //Asignacion para el archivo XMl
+        public string name { get; set; } //propiedad para el nombre
         [XmlAttribute("area")]
-        public decimal area { get; set; }
+        public decimal area { get; set; } //propiedad para el area
         [XmlAttribute("color")]
-        public string color { get; set; }
+        public string color { get; set; } //propiedad para el color 
     }
 }
